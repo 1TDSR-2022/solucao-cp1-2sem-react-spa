ee32b8e4a8026342bf8629e8b69ff6520c0cc88a
IgorEverton <rm95214@fiap.com.br>
2022-09-02 22:38:50 -0300

    criada a função de média e feita a estelização

--- rm95214/cp1/src/componentes/mediaAluno/MediaAluno.jsx
+++ rm95214/cp1/src/componentes/mediaAluno/MediaAluno.jsx
@@ -3,14 +3,30 @@ import React from "react";
 
 
 export default function MediaAluno(props){
+    
+    let media = 0
+
+    const objEstilo = {
+        color:"#000000",
+        fontSize: "2rem"
+    }
+
+    const calculaMedia = ()=>{
+        media = ((props.nota1 + props.nota2 + props.nota3)) / 3
+
+        if (media <= 0){
+            objEstilo.color = "#FF0000"
+        }else if 
+    }
+    
     return(
         <div>
             <h2>Componente MÉDIA-ALUNO</h2>
-            <div>
-                <p><strong>Aluno: {props.aluno}</strong></p>
-                <p><strong>Turma: {props.nota1}</strong></p>
-                <p><strong>RM: {props.nota2}</strong></p>
-                <p><strong>MÉDIA: {props.nota3}</strong></p>
+            <div className="row">
+                <p className="col-xm"><strong>Aluno: {props.aluno}</strong></p>
+                <p className="col-xm"><strong>Turma: {props.nota1}</strong></p>
+                <p className="col-xm"><strong>RM: {props.nota2}</strong></p>
+                <p className="col-xm"><strong>MÉDIA: {props.nota3}</strong></p>
             </div>
         </div>
     )
