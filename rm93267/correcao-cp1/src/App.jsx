174bf1d57f69708b06642256efcacf49d01e19e9
lucasmoraisnt <RM93267@fiap.com.br>
2022-09-02 21:59:30 -0300

    Declaração de notas e info alunos

--- rm93267/correcao-cp1/src/App.jsx
+++ rm93267/correcao-cp1/src/App.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { useState } from 'react'
 
 export default function App(){
+
+    //const objAluno ={
+    //    nome: 'joaquim',
+    //    rm: 99999,
+    //    turma: '1TDSZ'
+    //}
+
+    const[aluno, setAluno] = useState({
+        nome: 'joaquim',
+        rm: 99999,
+        turma: '1TDSZ'
+    })
+
+    
+    const nota1 = 6
+    const nota2 = 8
+    const nota3 = 9
+
     return(
         <>
-            <h1>CP1</h1>
+            <h1>CP1 de RWD 1º Semestre</h1>
         </>
     )
 }
