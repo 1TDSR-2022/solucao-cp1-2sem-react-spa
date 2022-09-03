43f4d6202ccb1ec17f623efea3e82bd5fcd3d634
IgorEverton <rm95214@fiap.com.br>
2022-09-02 22:00:02 -0300

    useState usado para seta objeto aluno

--- rm95214/cp1/src/App.jsx
+++ rm95214/cp1/src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function App(){
+    
+    //const objAluno = {
+        //nome: "Joaquim",
+        //rm : 999999,
+        //turma : "1tdsz"
+        //}
+    const nota1 = 0
+    const nota2 = 5
+    const nota3 = 7
+
+    const [aluno, setAluno] = useState({
+        nome: "Joaquim",
+        rm : 999999,
+        turma : "1tdsz"
+    })
+
+    useEffect(()=> {
+        setAluno(objAluno)
+    }, [])
+  
+    
     return(
-        <div>
-            
-        </div>
+        <>
+            <h1>CP1 RWD 1° SEMESTRE</h1>
+        </>
     )
 }
